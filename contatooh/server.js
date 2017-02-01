dc9b721efefe8c33a72023cd5c1b89bca075b0e1
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-01-31 23:00:33 -0200

    Add require express

--- contatooh/server.js
+++ contatooh/server.js
@@ -1,5 +1,7 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Sou um servidor criado pelo Node.js!\n');
-}).listen(3000, '127.0.0.1');
+var app = require('./config/express')();
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express Server escutando na porta ' +
+  app.get('port'));
+});
