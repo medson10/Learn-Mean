763da4131a771ada40d515044ad53bf2fc235c8f
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-02 15:26:50 -0200

    Refactored: Add argument

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,27 +1,14 @@
 angular.module('contatooh').controller('ContatosController',
-  function($scope) {
+  function($scope, $http) {
+    $scope.contatos = [];
     $scope.total = 0;
-    $scope.incrementa = function() {
-      $scope.total++;
-    };
-
-    $scope.contatos = [
-      {
-        "_id": 1,
-        "nome": "Contato Angular 1",
-        "email": "cont1@empresa.com.br"
-      },
-      {
-        "_id": 2,
-        "nome": "Contato Angular 2",
-        "email": "cont2@empresa.com.br"
-      },
-      {
-        "_id": 3,
-        "nome": "Contato Angular 3",
-        "email": "cont3@empresa.com.br"
-      }
-    ];
-
     $scope.filtro = '';
+    $http.get('/contatos')
+    .success(function(data) {
+      $scope.contatos = data;
+    })
+    .error(function(statusText) {
+      console.log("Não foi possível obter a lista de contatos");
+      console.log(statusText);
+    });
 });
