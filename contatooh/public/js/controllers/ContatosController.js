af7ada2306ce0f172e361169bbd984f3d75d36bd
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-02 00:27:09 -0200

    Add scope.filter

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -4,4 +4,24 @@ angular.module('contatooh').controller('ContatosController',
     $scope.incrementa = function() {
       $scope.total++;
     };
+
+    $scope.contatos = [
+      {
+        "_id": 1,
+        "nome": "Contato Angular 1",
+        "email": "cont1@empresa.com.br"
+      },
+      {
+        "_id": 2,
+        "nome": "Contato Angular 2",
+        "email": "cont2@empresa.com.br"
+      },
+      {
+        "_id": 3,
+        "nome": "Contato Angular 3",
+        "email": "cont3@empresa.com.br"
+      }
+    ];
+
+    $scope.filtro = '';
 });
