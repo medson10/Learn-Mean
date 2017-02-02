a8b1437f65f17ede1843785b9526b2422efb78e4
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-02 15:34:02 -0200

    Add module ngResource

--- contatooh/public/js/main.js
+++ contatooh/public/js/main.js
@@ -1,8 +1,8 @@
-angular.module('contatooh', ['ngRoute']).config(function($routeProvider) {
+angular.module('contatooh', ['ngRoute','ngResource']).config(function($routeProvider) {
 
   $routeProvider.when('/contatos', {
     templateUrl: 'partials/contatos.html',
     controller: 'ContatosController'
   });
-  
+
 });
