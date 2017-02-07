d958a06c945716815999ad0582087b54f3f3bf90
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-06 22:33:47 -0200

    Add method post to route contatos

--- contatooh/app/routes/contato.js
+++ contatooh/app/routes/contato.js
@@ -1,8 +1,9 @@
 module.exports = function(app) {
   var controller = app.controllers.contato;
-  
+
   app.route('/contatos')
-    .get(controller.listaContatos);
+    .get(controller.listaContatos)
+    .post(controller.salvaContato);
 
   app.route('/contatos/:id')
     .get(controller.obtemContato)
