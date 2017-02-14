37f6e9a47d540c43bbe081b3706da06f24931fcb
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-14 16:17:16 -0200

    Add sanitize and object dados

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
   var Contato = app.models.contato;
   var controller = {};
+  var sanitize = require('mongo-sanitize');
 
   controller.listaContatos = function(req, res) {
     var promise = Contato.find().populate('emergencia').exec()
@@ -29,7 +30,7 @@ module.exports = function (app) {
     );
   };
   controller.removeContato = function(req, res) {
-    var _id = req.params.id;
+    var _id = sanitize(req.params.id);
     Contato.remove({"_id" : _id}).exec()
     .then(
       function() {
@@ -42,10 +43,14 @@ module.exports = function (app) {
   };
   controller.salvaContato = function(req, res) {
     var _id = req.body._id;
-    req.body.emergencia = req.body.emergencia || null;
+    var dados = {
+      "nome" : req.body.nome,
+      "email" : req.body.email,
+      "emergencia" : req.body.emergencia || null
+    };
 
     if(_id) {
-      Contato.findByIdAndUpdate(_id, req.body).exec()
+      Contato.findByIdAndUpdate(_id, dados).exec()
       .then(
         function(contato) {
           res.json(contato);
@@ -56,7 +61,7 @@ module.exports = function (app) {
         }
       );
     } else {
-      Contato.create(req.body)
+      Contato.create(dados)
       .then(
         function(contato) {
           res.status(201).json(contato);
