fbff85e1b929fbf3a8162d27e37e9096ad18062a
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-14 16:17:58 -0200

    Add middlewares helmet and route 404

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+helmet = require('helmet');
 
 module.exports = function() {
   var app = express();
@@ -26,11 +27,20 @@ module.exports = function() {
   ));
   app.use(passport.initialize());
   app.use(passport.session());
+  app.use(helmet.xframe());
+  app.use(helmet.xssFilter());
+  app.use(helmet.nosniff());
+  app.disable('x-powered-by');
 
   load('models', {cwd: 'app'})
-    .then('controllers')
-    .then('routes')
-    .into(app);
+  .then('controllers')
+  .then('routes/auth.js')
+  .then('routes')
+  .into(app);
+
+  app.get('*', function(req, res) {
+    res.status(404).render('404');
+  });
 
   return app;
 };
