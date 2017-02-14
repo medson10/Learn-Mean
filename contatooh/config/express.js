a8374c588621cf550b90984781de95dacc6a5452
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-14 01:56:38 -0200

    Add middlewares cookieParser, session, passport.initialize and passport.session

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function() {
   var app = express();
@@ -14,11 +17,20 @@ module.exports = function() {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(require('method-override')());
+  app.use(cookieParser());
+  app.use(session(
+    { secret: 'homem avestruz',
+    resave: true,
+    saveUninitialized: true
+    }
+  ));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   load('models', {cwd: 'app'})
     .then('controllers')
     .then('routes')
     .into(app);
-    
+
   return app;
 };
